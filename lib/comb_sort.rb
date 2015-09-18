4be52882cee5427e64eb5a07372912761d897e63
Daniel Imms <tyriar@tyriar.com>
2015-09-18 00:42:15 -0700

    Add comb sort
    Fixes #5

---
+++ lib/comb_sort.rb
@@ -0,0 +1,21 @@
+module CombSort
+  def sort(array, compare = lambda { |a, b| a <=> b })
+    gap = array.length
+    shrink_factor = 1.3
+    swapped = false
+    while gap > 1 || swapped
+      if gap > 1
+        gap = (gap / shrink_factor).floor
+      end
+      swapped = false
+      i = 0
+      while gap + i < array.length
+        if compare.call(array[i], array[i + gap]) > 0
+          array[i], array[i + gap] = array[i + gap], array[i]
+          swapped = true
+        end
+        i += 1
+      end
+    end
+  end
+end
---
+++ test/comb_sort_tests.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require_relative '../lib/comb_sort'
+require_relative 'test_helper'
+
+class CombSortTest < Minitest::Test
+  extend TestHelper
+  include CombSort
+
+  add_all_base_tests
+end
