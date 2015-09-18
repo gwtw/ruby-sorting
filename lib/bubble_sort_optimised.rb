12f2bd31ecf501d648b22f1978cd9c7938ee15a4
Daniel Imms <tyriar@tyriar.com>
2015-09-18 00:56:33 -0700

    Add bubble sort optimised
    Fixes #2

---
+++ lib/bubble_sort_optimised.rb
@@ -0,0 +1,15 @@
+module BubbleSortOptimised
+  def sort(array, compare = lambda { |a, b| a <=> b })
+    unsorted_below = array.length
+    while unsorted_below != 0
+      last_swap = 0
+      for i in 1..(unsorted_below - 1)
+        if compare.call(array[i - 1], array[i]) > 0
+          array[i], array[i - 1] = array[i - 1], array[i]
+          last_swap = i
+        end
+      end
+      unsorted_below = last_swap
+    end
+  end
+end
---
+++ test/bubble_sort_optimised.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require_relative '../lib/bubble_sort_optimised'
+require_relative 'test_helper'
+
+class BubbleSortOptimisedTest < Minitest::Test
+  extend TestHelper
+  include BubbleSortOptimised
+
+  add_all_base_tests
+end
